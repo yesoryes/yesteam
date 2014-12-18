2fbb97792afac08e3989be1259b5751fe1862c92
thananyes <thanan.yes@gmail.com>
2014-12-18 18:20:41 +0530

    Thanan - Portfolio1

---
+++ Website Yes/Stage/Portfolio/js/portfolio.js
@@ -0,0 +1,145 @@
+
+
+$(document).ready(function(){
+
+
+
+
+
+// Add images	
+var project = [];
+$('.portWarp img').each(function(index) {
+	var img ="<div class='project'><div class='templates'><a href='#'><img src='"+ $('.portWarp').find('img:eq('+index+')').attr('src') +"'></a></div><div class='templateHover'><a href='#'><img src='images/arrows.png'><h2>Website<br>Development</h2><div class='templateName'><h3>Be On The Scene</h3></div></a></div></div>";
+	project.push(img);
+});
+
+
+function loadPort() {
+	
+	//Cont width
+	
+	var pWidth = 230; //$(".project").outerWidth(true)
+	var pHeight = 203; //$(".project").outerHeight(true)
+
+	var contWidth = $(".contentArea").width()-pWidth-40;
+	//$('.templateContent').css("max-width",contWidth +"px"); //foget why
+	$('.templateContent').css("width",contWidth +"px");
+	$('.heading').css("width",contWidth +"px");
+	$('.footer').css("width",contWidth +"px");
+	$('#screens').css("width",contWidth +"px");
+	
+	var contHeight = $(window).height() - $(".header").outerHeight(true) - $(".footer").outerHeight(true)-80;
+	$('.templateContent').css("height",contHeight +"px");
+	
+	
+	//Check count
+	var perRow = parseInt($(".templateContent").width()/pWidth);
+	var perCol = parseInt($(".templateContent").height()/pHeight);
+	var perSlide = perRow*perCol;
+	
+	
+	
+	//Load into Array
+	var proj = [];
+	var tempArr = [];
+	for (var i=0;i<project.length;i++) {
+		tempArr.push(project[i]);
+		if(tempArr.length>=perSlide) {
+			proj.push(tempArr);
+			tempArr=[];
+		}
+	};
+	if(tempArr.length>0) {
+		proj.push(tempArr);
+		tempArr=[];
+	}
+	//console.table(proj);
+	//console.log("proj="+proj[1].length);
+	
+	
+	//Load images
+	$('#portSlide0').remove();
+	var curProj = 0;
+	for (var i=0;i<proj.length;i++) {			
+			var emptyTemplate = "<li class='templateContent' id='portSlide"+i+"'><div class='portWarp'></div></li>";
+			$("#screens ul").append(emptyTemplate);
+			for (var j=0;j<proj[i].length;j++) {
+				var toApd = "#portSlide"+i+" .portWarp";
+				//alert("dd");
+				$(toApd).append(project[curProj]);
+				curProj++;
+			}
+	}
+	
+	
+	
+	//Call slider plug-in
+	if(proj.length>1) {
+		$('head').append('<link href="css/bjqs.css" type="text/css" rel="stylesheet">');
+		$.getScript("http://yesoryes.co.in//tests/tmp1/bjqs-1.3.min.js", function() {
+			$('#screens').bjqs({
+				width       : contWidth,
+				height      : contHeight,
+			  });
+		});
+	};
+	
+	
+	//centering (here coz, plugin overwrite)
+	$('.portWarp').css("width",(pWidth*perRow) +"px");
+	$('.portWarp').css("height",(pHeight*perCol) +"px");
+
+	
+	
+
+};
+loadPort();
+
+
+function clearPortfoio() {
+	$("#screens").html('');
+	$("#screens").removeAttr('style');
+	$(".contentArea").removeAttr('style');
+	$(".heading").removeAttr('style');
+	$(".footer").removeAttr('style');
+	var initTemplate = "<ul class='bjqs'><li class='templateContent' id='portSlide0'><div class='portWarp'></div></li></ul>";
+	$("#screens").append(initTemplate);
+	loadPort();
+}
+
+var resizeTimer;
+$( window ).resize(function() {
+	clearTimeout(resizeTimer);
+	if (!(navigator.userAgent.match(/msie/i) || navigator.userAgent.match(/trident/i)) ){ //checking IE
+	    resizeTimer = setTimeout(clearPortfoio, 100);
+	}
+});
+
+
+
+
+//Over fx
+$('.project').hover(
+  function() {
+   $(this).children('.templateHover').fadeIn('slow');
+   $(this).children('.templates').children('a').children('img').addClass('animScale');
+   $(this).children('.templateHover').find('.templateName').stop(true).animate({'bottom': 0}, {queue: false, duration: 300, easing:"swing"});
+   $(this).children('.templateHover').children('a').stop(true).animate({'borderWidth': 10}, {queue: false, duration: 300, easing:"swing"});
+   
+  }, function() {
+	$(this).children('.templateHover').fadeOut();
+	$(this).children('.templates').children('a').children('img').removeClass('animScale');
+	$(this).children('.templateHover').find('.templateName').stop(true).animate({'bottom': -40}, {queue: false, duration: 300, easing:"swing"});
+   $(this).children('.templateHover').children('a').stop(true).animate({'borderWidth': 0}, {queue: false, duration: 300, easing:"swing"});
+  }
+);
+ 
+/*$('.project').on('mouseenter mouseleave', function(e) {
+    $('.project').children('.templates').children('a').children('img').trigger(e.type);
+});*/
+
+
+
+
+});
+
---
+++ Website Yes/Stage/Portfolio/js/t1.js
@@ -0,0 +1,1 @@
+var sss= "ssssS";
