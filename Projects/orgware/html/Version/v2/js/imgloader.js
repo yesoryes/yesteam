a706235523a732ec2644ddc7c74c824b1c441a13
jadoram <ramprbh@gmail.com>
2015-07-01 21:29:06 +0530

    orgware updated

---
+++ Projects/orgware/html/Version/v2/js/imgloader.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(window).load( function(){
+    /* Preload code goes here */
+
+$.preloadImages = function() {
+  for (var i = 0; i < arguments.length; i++) {
+    $("<img />").attr("src", arguments[i]);
+  }
+}
+
+$.preloadImages("../img/4way-color.png","../img/4way-taxi-color.png","../img/acclary-color.png","../img/asna-color.png","../img/banner-link.png","../img/banner-link1024.png","../img/banner-mob-link.png","../img/paradise-color.png","../img/product1-hover.png","../img/product2-hover.png","../img/product3-hover.png","../img/product4-hover.png","../img/right-nav.png","../img/seskhan-link.png","../img/product1-1024.png","../img/product2-1024.png","../img/product3-1024.png","../img/product4-1024.png"); 
+});
--- Projects/orgware/html/web/js/imgloader.js
+++ Projects/orgware/html/web/js/imgloader.js
@@ -8,5 +8,5 @@ $.preloadImages = function() {
   }
 }
 
-$.preloadImages("../img/4way-color.png","../img/4way-taxi-color.png","../img/acclary-color.png","../img/asna-color.png","../img/banner-link.png","../img/banner-link1024.png","../img/banner-mob-link.png","../img/paradise-color.png","../img/product1-hover.png","../img/product2-hover.png","../img/product3-hover.png","../img/product4-hover.png","../img/right-nav.png","../img/seskhan-link.png","../img/product1-1024.png","../img/product2-1024.png","../img/product3-1024.png","../img/product4-1024.png"); 
+$.preloadImages("img/4way-color.png", "img/menu_option.png", "img/4way-taxi-color.png","img/acclary-color.png","img/asna-color.png","img/banner-link.png","img/banner-link1024.png","img/banner-mob-link.png","img/paradise-color.png","img/product1-hover.png","img/product2-hover.png","img/product3-hover.png","img/product4-hover.png","img/right-nav.png","img/seskhan-link.png","img/product1-1024.png","img/product2-1024.png","img/product3-1024.png","img/product4-1024.png"); 
 });
