9ee9eb9e1bf699d60f60ae57eb476cd7592e1d3d
jadoram <ramprbh@gmail.com>
2015-08-17 19:37:05 +0530

    orgware

--- Projects/orgware/html/web/js/main.js
+++ Projects/orgware/html/web/js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
 	
 	/* For Browser Height*/ 
-	$("header .carousel-inner .item").css({
+	 $("header .carousel-inner .item.one, header .carousel-inner .item.two, header .carousel-inner .item.three, header .carousel-inner .item.four, header .carousel-inner .item.five, header .carousel-inner .item.six").css({
 			'height' : $( window ).height() 
-	});
-	
-	 
+	}); 
+	  
 	
 }); 
 /*Menu More Button*/
@@ -28,7 +27,7 @@ $(document).ready(function() {
    
   });    
   / Then push them back /   
-  $('.more-close').click(function() {
+  $('.more-close, .black-overlay').click(function() {
      $('.menu').animate({right: "-325px"}, 500);
      $('body').animate({right: "0px"  }, 500); 
      $('.bg, .main-nav ul, .navbar-brand img, nav').slideDown(500);	 
@@ -71,32 +70,8 @@ $('.menu, .black-overlay').css({
 			return false;
 		});
 	});
-	
- 
-
- 
+	 
 /*
-	
-$(window).load(function()
-{
-		
-	$('#preloader').fadeOut('slow',function(){$(this).remove();}); 
-		 
-	$.preloadImages = function()
-	  {
-			  for (var i = 0; i < arguments.length; i++)
-			  {
-				$("<img />").attr("src", arguments[i]);
-	          }
-      }
-
-                $.preloadImages("img/4way-color.png", "img/menu_option.png", "img/4way-taxi-color.png","img/acclary-color.png","img/asna-color.png","img/banner-link.png","img/banner-link1024.png","img/banner-mob-link.png","img/paradise-color.png","img/product1-hover.png","img/product2-hover.png","img/product3-hover.png","img/product4-hover.png","img/right-nav.png","img/seskhan-link.png","img/product1-1024-hover.png","img/product2-1024-hover.png","img/product3-1024-hover.png","img/product4-1024-hover.png", "img/service1-hover.png", "img/service2-hover.png", "img/service3-hover.png", "img/service4-hover.png","img/service5-hover.png", "img/service6-hover.png", "img/service7-hover.png", "img/service8-hover.png", "img/page-top-hover.png", "img/service9-hover.png", "img/service10-hover.png", "img/menu_option.png", "img/product-main.jpg', "img/product-main-1024.jpg', "img/google-hover.png", "img/google-menu-hover.png", "img/facebook-hover.png", "img/facebook-menu-hover.png", "img/linked-hover.png", "img/linked-menu-hover.png",  "img/product1-mob-hover.png", "img/product2-mob-hover.png", "img/product3-mob-hover.png", "img/product4-mob-hover.png", "img/youtube-hover.png", "img/youtube-menu-hover.png", "img/web-app-menu-hover.png", "img/mobile-app-hover.png", "img/mobile-game-hover.png"); 
-});*/
- 
-
- 
- 
- 
  
 /* Prodcut Animation */
 
